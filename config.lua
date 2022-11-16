bed23a9c2ef5d055f72b302a7241663446a8e9a0
Hakos47 <76844995+hakos47@users.noreply.github.com>
2022-11-16 11:25:39 +0100

    Add new info config
    log start server config.
    add https connect config

--- config.lua
+++ config.lua
@@ -2,6 +2,8 @@ Config = {}
 Config.loggins = true -- Activar o desactivar el loggin de los jugadores
 Config.resources = true -- Activar o desactivar para ver si se reinicia algun recursos
 Config.kills = true -- Activar o desactivar para ver las meurtes de los jugadores
+Config.startServer = false -- Activar o desactivar para ver cuando se inicia el servidor
+Config.connect = 'your https: connect'
 
 Config.licenses = {
      need = {
